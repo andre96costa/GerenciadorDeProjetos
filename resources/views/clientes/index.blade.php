9a61e1548cc833a0a08088854b931c509f04bfa0
André Costa <andre2396costa@gmail.com>
2024-05-09 20:18:07 -0300

    Setup do projeto

---
+++ resources/views/clientes/index.blade.php
@@ -0,0 +1,53 @@
+<x-layout titulo="Lista de Clientes">
+    <div class="flex justify-end my-3">
+        <a class="bg-green-500 border rounded-md p-1 px-3 text-white" href="/clientes/create">Criar cliente</a>
+    </div>
+
+    <div class="relative overflow-x-auto">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        Nome
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Endereço
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Descrição
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Projetos
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($clientes as $cliente)
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <th scope="row"
+                            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{ $cliente->nome }}
+                        </th>
+                        <td class="px-6 py-4">
+                            {{ $cliente->endereco }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ $cliente->descricao }}
+                        </td>
+                        <td class="px-6 py-4">
+                            @forelse ($cliente->projects as $projeto)
+                                @if ($loop->last)
+                                    {{ $projeto->nome }}
+                                @else
+                                    {{ $projeto->nome }}{{ ', ' }}
+                                @endif
+                            @empty
+                                Nenhum projeto
+                            @endforelse
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</x-layout>
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\SiteController;
+use App\Http\Controllers\VerServicos;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded within the "web" middleware group which includes
+| sessions, cookie encryption, and more. Go build something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('clientes', [ClienteController::class, 'index']);
+Route::get('clientes/create', [ClienteController::class, 'create']);
+Route::post('clientes', [ClienteController::class, 'store']);
